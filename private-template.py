703bde8a4b5793fb5cdaf9dc6f62971d3f149e05
Arjen de Vries <arjen@acm.org>
2017-02-19 22:01:39 +0100

    Added template for private.py Added settings.py in queries

---
+++ private-template.py
@@ -0,0 +1,12 @@
+# Application Settings:
+TWITTER_APP_KEY    = "YOUR Consumer Key (API Key)"
+TWITTER_APP_SECRET = "YOUR Consumer Secret (API Secret)"
+
+# Access Tokens:
+TWITTER_KEY        = "YOUR Access Token"
+TWITTER_SECRET     = "YOUR Access Token Secret"
+
+# Database Location 
+#   use local disk, e.g., /scratch/${USERNAME}/
+DATA_DIR           = "/YOUR DATA DIRECTORY/"
+CONNECTION_STRING  = "sqlite:///" + DATA_DIR + "tweets.db"
---
+++ queries/settings.py
@@ -0,0 +1,8 @@
+CONNECTION_STRING = ""
+CSV_NAME = "tweets.csv"
+TABLE_NAME = "verkiezingen"
+
+try:
+    from private import *
+except Exception:
+    pass
